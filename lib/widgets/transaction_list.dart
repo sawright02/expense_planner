a557ea15af9949d73b4dc6601a07002205faba62
Shane Wright <sawright02@gmail.com>
2019-09-20 11:35:35 -0400

    added media queries

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,59 +11,57 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 600,
-      child: transactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text(
-                  'No transactions added yet!',
-                  style: Theme.of(context).textTheme.title,
+    print('device height: ${MediaQuery.of(context).size.height}');
+    return transactions.isEmpty
+        ? Column(
+            children: <Widget>[
+              Text(
+                'No transactions added yet!',
+                style: Theme.of(context).textTheme.title,
+              ),
+              SizedBox(height: 20),
+              Container(
+                child: Image.asset('assets/images/waiting.png'),
+                height: 200,
+              ),
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(
+                  horizontal: 5,
+                  vertical: 8,
                 ),
-                SizedBox(height: 20),
-                Container(
-                  child: Image.asset('assets/images/waiting.png'),
-                  height: 200,
-                ),
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 5,
-                    vertical: 8,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text('\$${transactions[index].amount}'),
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => deleteTransaction(
-                        transactions[index].id,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Text('\$${transactions[index].amount}'),
                       ),
                     ),
                   ),
-                );
-              },
-              itemCount: transactions.length,
-            ),
-    );
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () => deleteTransaction(
+                      transactions[index].id,
+                    ),
+                  ),
+                ),
+              );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
